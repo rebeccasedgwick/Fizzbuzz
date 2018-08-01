cff99dc774c6f4329eff9d9eab6c6c2293e03fcd
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-08-02 00:00:34 +0100

    Added working Fizzbuzz method on Integer class

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,17 @@
-def fizzbuzz(number)
-  "fizz"
+class Integer
+
+  def fizzbuzz
+
+    if self % 3 == 0 && self % 5 == 0
+      "fizzbuzz"
+    elsif self % 3 == 0
+      "fizz"
+    elsif self % 5 == 0
+      "buzz"
+    else
+      self
+    end
+
+  end
+
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,6 +2,23 @@ require "./lib/fizzbuzz"
 
 describe "fizzbuzz" do
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq "fizz"
+    expect(3.fizzbuzz).to eq "fizz"
   end
+
+  it 'returns "buzz" when passed 5' do
+    expect(5.fizzbuzz).to eq "buzz"
+  end
+
+  it 'returns "fizzbuzz" when passed 30' do
+    expect(30.fizzbuzz).to eq "fizzbuzz"
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(15.fizzbuzz).to eq "fizzbuzz"
+  end
+
+  it 'returns "4" when passed 4' do
+    expect(4.fizzbuzz).to eq 4
+  end
+
 end
